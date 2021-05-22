b893fe0515ff495070e1d7b6aa830cf318d800d5
mouhebDesigner <mouhebabderrahim@gmail.com>
2021-05-22 22:19:01 +0100

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'nom' => ['required', 'string', 'max:255'],
-            'prenom' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255'],
             'phone_number' => ['required', 'numeric', 'digits:8', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
